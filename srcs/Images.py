f16650b747b7d458718566695bd4236501a832b5
Takeshi Shikama <bamboo40@gmail.com>
2017-05-09 06:37:28 +0900

    リネームして保存する機能の実装完了

--- srcs/Images.py
+++ srcs/Images.py
@@ -33,37 +33,82 @@ class Images():
     def get_dist_dir(self,root_dir,day_date):
         #保存先を作成する root + 日付にする
         return root_dir + day_date[:4] + day_date[5:7] + day_date[8:10] + "/"
+
+    def create_copy_file_list(self):
+        src_files = []
+        dist_files = []
+
+        #コピーファイルリストを作成する.
+        for name,info in self.image_files.items():
+            src_name = name
+            dist_name = info["dist_dir"]+info["file_name"]
+
+            if self.is_same_file(src_name,dist_name):
+                continue
+
+            src_files.append(src_name)
+            dist_files.append(dist_name)
+        
+        #コピーファイルリストのうち、同じ名前のものをリネームする.
+        #リネームした後のファイル名がかぶらない用にリネーム後listの最後に追加して再チェックする
+        is_check = True
+        
+        while is_check:
+            is_check = False
+            for src_name,dist_name in zip(src_files,dist_files):
+
+                if self.is_same_file(src_name,dist_name):
+                    #リネームしたファイルで同じものがある場合は、削除する.
+                    src_files.remove(src_name)
+                    dist_files.remove(dist_name)
+                else:
+                    if os.path.exists(dist_name):
+                        #元のファイルをリストから削除してリネームしてlistの最後に追加する
+                        name_no_ext,ext = os.path.splitext(dist_name)            
+        
+                        src_files.remove(src_name)
+                        dist_files.remove(dist_name)
+                        
+                        dist_name = name_no_ext + "(2)" + ext
+                        src_files.append(src_name)
+                        dist_files.append(dist_name)
+                        is_check = True
+                    else:
+                        continue
+                    
+        
+        return src_files,dist_files
+        
+    def is_same_file(self,src,dist):
+        is_same = False
+        if os.path.exists(dist):
+            #サイズが同じ
+            if os.path.getsize(dist) == os.path.getsize(src):
+                is_same = True
+        return is_same
         
     def copy_files_to_dist_dir(self):
         #保存先にファイルをコピーする
         #同じファイルはコピーしない.
         print("Copy Start",len(self.image_files),"Files ...")
         count = 0
+
+        #ディレクトリが存在しなければ作成する
         for name,info in self.image_files.items():
-            #ディレクトリが存在しなければ作成する
             if not os.path.isdir(info["dist_dir"]):
                 os.makedirs(info["dist_dir"])
-            
-            #同じファイルがあれば、サイズが異なる場合のみ別名でコピーする.
-            dist_name = info["dist_dir"]+info["file_name"]
-
-            if os.path.exists(dist_name):
-                #サイズが同じ場合、コピーしない.
-                if os.path.getsize(dist_name) == os.path.getsize(name):
-                    continue
-                else:
-                    #サイズが異なる場合、別名で保存する.
-                    #TODO コピー後のファイルもチェックする必要あり
-                    name_no_ext,ext = os.path.splitext(dist_name)            
-                    dist_name = name_no_ext + "(2)" + ext
-
+                
+        src_files,dist_files = self.create_copy_file_list()
+        
+        for src,dist in zip(src_files,dist_files):
             try:
-                shutil.copy2(name,dist_name)
+                shutil.copy2(src,dist)
+                #DEBUG
+                #print(src,dist)
                 count+=1
 
             except shutil.SameFileError as err:
                 continue
-            
-            
+
         print(count,"Files Finished")
         
--- srcs/main.py
+++ srcs/main.py
@@ -8,10 +8,16 @@ Created on Sat May  6 10:23:50 2017
 #import os
 #os.path.append("./")
 from Images import Images
+import time
 
 if __name__ == "__main__":
     image_directory = "/Users/take/Pictures/flickr/"
+    start = time.time()
+    print("Read Images")
     imgs = Images(image_directory)
-    imgs.disp_info()
+    print("Elaped TIme",time.time()-start,"[s]")
+    #imgs.disp_info()
     
+    print("Copy Images")
     imgs.copy_files_to_dist_dir()
+    print("Elaped TIme",time.time()-start,"[s]")
